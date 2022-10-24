0cf8fe2e03128bee83c75f8628701ce932495b20
wooryung <wooryung1004@naver.com>
2022-10-24 15:57:28 +0900

    큐(Queue) 구현하기

---
+++ Chapter5/ch05/MyListQueueTest.java
@@ -0,0 +1,18 @@
+package ch05;
+
+public class MyListQueueTest {
+
+	public static void main(String[] args) {
+
+		MyListQueue listQueue = new MyListQueue();
+		listQueue.enQueue("A");
+		listQueue.enQueue("B");
+		listQueue.enQueue("C");
+		
+		listQueue.printQueue();
+		
+		System.out.println(listQueue.deQueue());
+		System.out.println(listQueue.deQueue());
+	}
+
+}
